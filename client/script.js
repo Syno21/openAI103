73d02dc0364a4391c2c8cc350b1be37a4c0c47e4
unknown <Admin@MUTR1611.mut.ac.za>
2023-03-24 14:04:07 +0200

    upload on the server

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv); 
 
-    const response = await fetch('https://openai103media.onrender.com', {
+    const response = await fetch('https://one03openai.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
